482e8f540a02ea9bb6e7d55ea126850f70149348
René Léveillé <rene.leveille@polymtl.ca>
2017-09-08 23:03:37 -0400

    consent document contents

--- Camelot/ConsentDocument.swift
+++ Camelot/ConsentDocument.swift
@@ -14,7 +14,26 @@ public var ConsentDocument: ORKConsentDocument {
     let consentDocument = ORKConsentDocument()
     consentDocument.title = "Example Consent"
     
-    //TODO: consent sections
+    // consent sections
+    let consentSectionTypes: [ORKConsentSectionType] = [
+        .overview,
+        .dataGathering,
+        .privacy,
+        .dataUse,
+        .timeCommitment,
+        .studySurvey,
+        .studyTasks,
+        .withdrawing
+    ]
+    
+    var consentSections: [ORKConsentSection] = consentSectionTypes.map { contentSectionType in
+        let consentSection = ORKConsentSection(type: contentSectionType)
+        consentSection.summary = "If you wish to complete this study..."
+        consentSection.content = "In this study you will be asked five, wait, no, three! questions. You will also have your voice recorded for ten seconds."
+        return consentSection
+    }
+    
+    consentDocument.sections = consentSections
     
     //TODO: signature
     
