e97246def762a4892ba4c41b826d9d313efbdc7b
René Léveillé <rene.leveille@polymtl.ca>
2017-09-09 09:54:38 -0400

    init surveyTask

---
+++ Camelot/SurveyTask.swift
@@ -0,0 +1,31 @@
+//
+//  SurveyTask.swift
+//  Camelot
+//
+//  Created by René Léveillé on 2017-09-09.
+//  Copyright © 2017 René Léveillé. All rights reserved.
+//
+
+import Foundation
+import ResearchKit
+
+public var SurveyTask: ORKOrderedTask {
+    var steps = [ORKStep]()
+    
+    //TODO: add instructions step
+    
+    
+    //TODO: add name question
+    
+    
+    //TODO: Add quest question
+    
+    
+    //TODO: add color question
+    
+    
+    //TODO: add summary step
+    
+    
+    return ORKOrderedTask(identifier: "SurveyTask", steps: steps)
+}
