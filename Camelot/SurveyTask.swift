08d2bb7faad93833e43fe24226b3898c4d50983f
René Léveillé <rene.leveille@polymtl.ca>
2017-09-09 10:02:07 -0400

    text input question

--- Camelot/SurveyTask.swift
+++ Camelot/SurveyTask.swift
@@ -18,8 +18,12 @@ public var SurveyTask: ORKOrderedTask {
     instructionStep.text = "Who would cross the Bridge of Death must answer me these questions three, ere the other side they see."
     steps += [instructionStep]
     
-    //TODO: add name question
-    
+    // add name question
+    let nameAnswerFormat = ORKTextAnswerFormat(maximumLength: 20)
+    nameAnswerFormat.multipleLines = false
+    let nameQuestionStepTitle = "What, is your name?"
+    let nameQuestionStep = ORKQuestionStep(identifier: "QuestionStep", title: nameQuestionStep, answer: nameAnswerFormat)
+    steps += [nameQuestionStep]
     
     //TODO: Add quest question
     
