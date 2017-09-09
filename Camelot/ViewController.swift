876c7e224429b4c7cbe8950dd48261696c535beb
René Léveillé <rene.leveille@polymtl.ca>
2017-09-08 23:50:20 -0400

    presenting the consent task

--- Camelot/ViewController.swift
+++ Camelot/ViewController.swift
@@ -21,6 +21,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func consentTapped(sender: AnyObject) {
+        let taskViewController = ORKTaskViewController(task: ConsentTask, taskRun: nil)
+        taskViewController.delegate = self
+        present(taskViewController, animated: true, completion: nil)
+    }
+}
 
+extension ViewController : ORKTaskViewControllerDelegate {
+    func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
+        // Handle results with taskViewController.result
+        taskViewController.dismiss(animated: true, completion: nil)
+    }
 }
 
